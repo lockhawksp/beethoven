7c18cbf6dced0435537fb4067dfa878ae9ccc6af
Cen Wang <lockhawkspersonal@gmail.com>
2014-10-12 20:36:06 +0800

    Create profile if user is created

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
@@ -7,3 +8,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.get_full_name() or self.user.username
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+post_save.connect(create_user_profile, sender=User)
