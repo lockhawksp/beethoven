d4dfb5f0fa79bbe71c5c74f6a254ef3b6bf30795
Cen Wang <lockhawkspersonal@gmail.com>
2014-10-25 23:28:42 +0800

    Test user profile is automatically created

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from accounts.models import Profile
+
+
+class AccountTests(TestCase):
+
+    def setUp(self):
+        User.objects.create_user('user', password='0')
+
+    def test_profile_is_created_after_user_is_created(self):
+        Profile.objects.get(user__username='user')
